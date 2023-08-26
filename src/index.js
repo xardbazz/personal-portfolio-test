70e8d5e92211cfae708af4c262846fbf88e24495
xardbazz <Official.kamalissa@gmail.com>
2023-08-26 12:37:12 +0300

    Updated home page

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import App from './pages/Home';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+  </React.StrictMode>
+);
+root.render(
+  <React.StrictMode>
+    <Home />
   </React.StrictMode>
 );
 
